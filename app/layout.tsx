9c6c6f0a1cbed32c0d9497fc1877ceb313d2ef84
Majid Tahir <mtahir@gmail.com>
2026-07-10 11:58:42 -0500

    feat: pre-paint theme script + ThemeProvider in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { grotesk, inter, mono } from "./fonts";
 import Sidebar from "@/components/layout/Sidebar";
 import MobileNav from "@/components/layout/MobileNav";
 import { getSessionUser } from "@/lib/session";
+import { THEME_INIT_SCRIPT } from "@/lib/theme";
+import ThemeProvider from "@/components/theme/ThemeProvider";
 
 export const metadata: Metadata = {
   title: { default: "Tracker", template: "%s · Tracker" },
@@ -15,21 +17,30 @@ export default async function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   const user = await getSessionUser();
   return (
-    <html lang="en" className={`${inter.variable} ${grotesk.variable} ${mono.variable}`}>
+    <html
+      lang="en"
+      className={`${inter.variable} ${grotesk.variable} ${mono.variable}`}
+      suppressHydrationWarning
+    >
+      <head>
+        <script dangerouslySetInnerHTML={{ __html: THEME_INIT_SCRIPT }} />
+      </head>
       <body className="min-h-dvh font-sans">
-        {user ? (
-          <>
-            <Sidebar username={user.displayUsername ?? user.username ?? user.name} />
-            <main className="lg:pl-60">
-              <div className="mx-auto max-w-[110rem] px-4 py-6 pb-24 lg:px-8 lg:py-8 lg:pb-8">
-                {children}
-              </div>
-            </main>
-            <MobileNav />
-          </>
-        ) : (
-          children
-        )}
+        <ThemeProvider>
+          {user ? (
+            <>
+              <Sidebar username={user.displayUsername ?? user.username ?? user.name} />
+              <main className="lg:pl-60">
+                <div className="mx-auto max-w-[110rem] px-4 py-6 pb-24 lg:px-8 lg:py-8 lg:pb-8">
+                  {children}
+                </div>
+              </main>
+              <MobileNav />
+            </>
+          ) : (
+            children
+          )}
+        </ThemeProvider>
       </body>
     </html>
   );
